3f485aff32d14644310d0159bf24b685876b2b7a
Brent Johnson <79657494+brentleejohnson@users.noreply.github.com>
2021-05-08 22:27:48 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 // This is my first JavaScript code
 console.log("Hello World");
+
+// My first var
+let name = "Brent";
+console.log(name);
+
+// Cannot be a reverved keyword
+// Should be meaningful
+// Cannot contain a number (1name)
+// Cannot contain a space or hyphen (-)
+// Are case-sensitive
+
+let firstName = "Brent";
+lastName = "Johnson";
+
+// My first constants
+// If you do not need to reassign a var, use const
+const interestRate = 0.3;
+
+// If you do need to reassign a var, use let
+// let interestRate = 0.3
+interestRate = 1;
+console.log(interestRate);
